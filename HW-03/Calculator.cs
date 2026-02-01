def6afd8170f9179930318fd65936f586ee8fa6f
Aleksandr <alekstark@live.ru>
2026-02-01 11:44:43 +0300

    1

---
+++ HW-03/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HW_03
+{
+    public interface ICalculator /// Интерфейс для калькулятора
+    {
+        int Sum(int a, int b);
+    }
+
+    public class Calculator() : ICalculator  ///Калькулятор на вход принимаем только целые числа (для добных нужно переделать на double)
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
--- HW-03/Program.cs
+++ HW-03/Program.cs
@@ -3,18 +3,6 @@ using static System.Console;
 
 namespace HW_03
 {
-    public interface ICalculator /// Интерфейс для калькулятора
-    {
-        int Sum(int a, int b);
-    }
-
-    public class Calculator() : ICalculator  ///Калькулятор на вход принимаем только целые числа (для добных нужно переделать на double)
-    {
-        public int Sum(int a, int b)
-        {
-            return a + b;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
