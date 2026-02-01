484b31e8b962ff4bf21878d0487f7eb2064962c2
Aleksandr <alekstark@live.ru>
2026-02-01 12:07:13 +0300

    1

--- HW-03/Program.cs
+++ HW-03/Program.cs
@@ -14,10 +14,12 @@ namespace HW_03
                 string inputA = ReadLine();
                 if(!int.TryParse(inputA, out int NumberOne))  // проверяем корректность ввода иначе выбрасываем ошибку
                     throw new FormatException("Неверный формат первого числа.");
+                
                 Write("Укажите второе целое число: ");
                 string inputB = ReadLine();
                 if (!int.TryParse(inputB, out int NumberTwo)) // проверяем корректность ввода иначе выбрасываем ошибку
                     throw new FormatException("Неверный формат второго числа.");
+                
                 int result = calculator.Sum(NumberOne,NumberTwo);
                 WriteLine(result);
 
